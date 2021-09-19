6a4f5edf89477143de481fc71720fb1984b721aa
Faisal Habib <faisal@simplusinnovation.com>
2021-09-19 12:48:36 +0500

    Added filters to community table

--- src/views/Community/parts/List.tsx
+++ src/views/Community/parts/List.tsx
@@ -51,6 +51,26 @@ export function List(props: Props) {
             </>)
     }
 
+    const filterInterests = (items: any) => {
+        const interestArray: any[] = [];
+        items.map((item: any) => item.interests.map((interest: any) => interestArray.push(interest)))
+        return (
+            [...new Set(interestArray) as any].map( (interests: any) => {return {
+                text: interests,
+                value: interests
+            }}))
+    }
+
+    const filterSkills = (items: any) => {
+        const skillsArray: any[] = [];
+        items.map((item: any) => item.skills.map((skill: any) => skillsArray.push(skill)))
+        return (
+            [...new Set(skillsArray) as any].map( (skills: any) => {return {
+                text: skills,
+                value: skills
+            }}))
+    }
+
 
     const columns = [
         {
@@ -72,6 +92,11 @@ export function List(props: Props) {
             title: 'Organization',
             dataIndex: 'company',
             key: 'company',
+            filters: [...new Set(data.map(item => item.company)) as any].map( (company: any) => {return {
+                text: company,
+                value: company
+            }}),
+            onFilter: (value: any, record: any) => record.company.indexOf(value) === 0,
             sorter: (a: any, b: any) => a.company.localeCompare(b.company),
 
         },
@@ -93,18 +118,27 @@ export function List(props: Props) {
             title: 'Country',
             dataIndex: 'country',
             key: 'country',
+            filters: [...new Set(data.map(item => item.country)) as any].map( (country: any) => {return {
+                text: country,
+                value: country
+            }}),
+            onFilter: (value: any, record: any) => record.country.indexOf(value) === 0,
             sorter: (a: any, b: any) => a.country.localeCompare(b.country),
         },
         {
             title: 'Interests',
             key: 'interests',
             dataIndex: 'interests',
+            filters: filterInterests(data),
+            onFilter: (value: any, record: any) => record.interests.includes(value),
             render: renderTags,
         },
         {
             title: 'Skills',
             key: 'skills',
             dataIndex: 'skills',
+            filters: filterSkills(data),
+            onFilter: (value: any, record: any) => record.skills.includes(value),
             render: renderTags,
         },
         {
@@ -119,7 +153,7 @@ export function List(props: Props) {
         },
     ];
 
-
+    console.log(data)
 
     return <>
         <Table
